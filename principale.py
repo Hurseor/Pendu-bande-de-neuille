bcfef7cbdc4538dabe39b7c4f1eef84e20748616
Hurseor <momo2304gm@gmail.com>
2025-05-15 22:47:08 +0200

    Update principale.py

--- principale.py
+++ principale.py
@@ -1,2 +1,8 @@
-print("yo")
-def amaury (list : tab) ->int
+import random
+
+# Ouvre le fichier en lecture
+with open("mot.txt") as fichier:
+    # Lit toutes les lignes et enlève les retours à la ligne
+    mot_choisi=random.choice([ligne.strip() for ligne in fichier])
+
+print("Mot choisi :", mot_choisi)
